66cc8be4fff3425bae725fbbbbd500f7eac85170
Daniel Robert Costa <35615346+drobcosta@users.noreply.github.com>
2019-12-04 16:13:15 -0300

    Update all code

--- projeto_final_estrutura.sql
+++ projeto_final_estrutura.sql
@@ -1,3 +1,13 @@
+DROP TABLE IF EXISTS produto_categoria CASCADE;
+DROP TABLE IF EXISTS produto CASCADE;
+DROP TABLE IF EXISTS cliente CASCADE;
+DROP TABLE IF EXISTS cliente_endereco CASCADE;
+DROP TABLE IF EXISTS cliente_telefone CASCADE;
+DROP TABLE IF EXISTS status CASCADE;
+DROP TABLE IF EXISTS pedido CASCADE;
+DROP TABLE IF EXISTS pedido_produto CASCADE;
+DROP TABLE IF EXISTS pedido_status CASCADE;
+
 CREATE TABLE IF NOT EXISTS produto_categoria (
 	id SMALLSERIAL NOT NULL,
 	nome VARCHAR(50) NOT NULL,
@@ -5,6 +15,13 @@ CREATE TABLE IF NOT EXISTS produto_categoria (
 	data_criacao TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT produto_categoria_pk PRIMARY KEY (id),
 	CONSTRAINT produto_categoria_unique_1 UNIQUE (nome)
+);
+
+CREATE OR REPLACE VIEW vw_produto_categoria AS (
+    SELECT  id,
+            nome,
+            ativo
+    FROM produto_categoria
 );
 
 CREATE TABLE IF NOT EXISTS produto (
@@ -19,6 +36,16 @@ CREATE TABLE IF NOT EXISTS produto (
 	CONSTRAINT produto_valor_check_1 CHECK (valor >= 0)
 );
 
+CREATE OR REPLACE VIEW vw_produto AS (
+    SELECT  numero_serie,
+            produto_categoria_id,
+            nome,
+            valor,
+            ativo
+    FROM produto
+    WHERE valor >= 0
+) WITH LOCAL CHECK OPTION;
+
 CREATE TABLE IF NOT EXISTS cliente (
 	cpfcnpj VARCHAR(14) NOT NULL,
 	nome VARCHAR(150) NOT NULL,
@@ -28,6 +55,15 @@ CREATE TABLE IF NOT EXISTS cliente (
 	data_criacao TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT cliente_pk PRIMARY KEY (cpfcnpj),
 	CONSTRAINT cliente_email_check_1 UNIQUE (email)
+);
+
+CREATE OR REPLACE VIEW vw_cliente AS (
+    SELECT  cpfcnpj,
+            nome,
+            email,
+            senha,
+            ativo
+    FROM cliente
 );
 
 CREATE TABLE IF NOT EXISTS cliente_endereco (
@@ -47,6 +83,20 @@ CREATE TABLE IF NOT EXISTS cliente_endereco (
 	CONSTRAINT cliente_fk_1 FOREIGN KEY (cliente_cpfcnpj) REFERENCES cliente(cpfcnpj)
 );
 
+CREATE OR REPLACE VIEW vw_cliente_endereco AS (
+    SELECT  cliente_cpfcnpj,
+            cep,
+            logradouro,
+            numero,
+            complemento,
+            bairro,
+            cidade,
+            referencia,
+            entrega,
+            ativo
+    FROM cliente_endereco
+);
+
 CREATE TABLE IF NOT EXISTS cliente_telefone (
 	id BIGSERIAL NOT NULL,
 	cliente_cpfcnpj VARCHAR(14) NOT NULL,
@@ -61,6 +111,16 @@ CREATE TABLE IF NOT EXISTS cliente_telefone (
 	CONSTRAINT cliente_telefone_unico UNIQUE (cliente_cpfcnpj,ddd,telefone,ramal)
 );
 
+CREATE OR REPLACE VIEW vw_cliente_telefone AS (
+    SELECT  cliente_cpfcnpj,
+            ddd,
+            telefone,
+            ramal,
+            melhor_horario,
+            ativo
+    FROM cliente_telefone
+);
+
 CREATE TABLE IF NOT EXISTS status (
 	id SMALLSERIAL NOT NULL,
 	nome VARCHAR(20) NOT NULL,
@@ -69,6 +129,12 @@ CREATE TABLE IF NOT EXISTS status (
 	CONSTRAINT status_pk PRIMARY KEY (id),
 	CONSTRAINT status_unique_1 UNIQUE (nome)
 );
+
+CREATE OR REPLACE VIEW vw_status AS (
+    SELECT id, nome, ativo
+    FROM status
+    WHERE ativo IS TRUE
+) WITH CASCADED CHECK OPTION;
 
 CREATE TABLE IF NOT EXISTS pedido (
 	id BIGSERIAL NOT NULL,
@@ -80,7 +146,16 @@ CREATE TABLE IF NOT EXISTS pedido (
 	CONSTRAINT pedido_pk PRIMARY KEY (id),
 	CONSTRAINT status_fk_1 FOREIGN KEY (status_id) REFERENCES status(id),
 	CONSTRAINT cliente_fk_1 FOREIGN KEY (cliente_cpfcnpj) REFERENCES cliente(cpfcnpj),
-	CONSTRAINT pedido_valor_check_1 CHECK (valor > 0)
+	CONSTRAINT pedido_valor_check_1 CHECK (valor >= 0)
+);
+
+CREATE OR REPLACE VIEW vw_pedido AS (
+    SELECT  status_id,
+            cliente_cpfcnpj,
+            valor,
+            data_ultima_atualizacao,
+            data_criacao
+    FROM pedido
 );
 
 CREATE TABLE IF NOT EXISTS pedido_produto (
@@ -99,48 +174,65 @@ CREATE TABLE IF NOT EXISTS pedido_produto (
 	CONSTRAINT pedido_produto_produto_valor_check_1 CHECK (produto_valor >= 0)
 );
 
-ALTER TABLE IF EXISTS pedido DROP CONSTRAINT status_fk_1;
+CREATE OR REPLACE VIEW vw_pedido_produto AS (
+    SELECT  pedido_id,
+            produto_numero_serie,
+            produto_categoria_id,
+            produto_nome,
+            produto_valor,
+            ativo,
+            data_criacao,
+            data_modificacao
+    FROM pedido_produto
+    WHERE produto_valor >= 0
+);
+
+DROP VIEW IF EXISTS vw_pedido;
+ALTER TABLE IF EXISTS pedido DROP CONSTRAINT IF EXISTS status_fk_1;
 ALTER TABLE IF EXISTS pedido DROP COLUMN IF EXISTS status_id;
+
+CREATE OR REPLACE VIEW vw_pedido AS (
+    SELECT  cliente_cpfcnpj,
+            valor,
+            data_ultima_atualizacao,
+            data_criacao
+    FROM pedido
+);
 
 CREATE TABLE IF NOT EXISTS pedido_status (
 	pedido_id BIGINT NOT NULL,
-	status_id SMALLINT NOT NULL,
+	status_id SMALLINT NOT NULL DEFAULT 1,
 	data_criacao TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT pedido_status_pk PRIMARY KEY (pedido_id,status_id),
 	CONSTRAINT pedido_fk_1 FOREIGN KEY (pedido_id) REFERENCES pedido(id),
 	CONSTRAINT status_fk_1 FOREIGN KEY (status_id) REFERENCES status(id)
 );
 
-INSERT INTO produto_categoria (nome) VALUES ('Eletrodomésticos');
-INSERT INTO produto_categoria (nome) VALUES ('Celulares');
-INSERT INTO produto_categoria (nome) VALUES ('Informática');
-INSERT INTO produto_categoria (nome) VALUES ('Cama, Mesa e Banho');
-INSERT INTO produto_categoria (nome) VALUES ('Música');
-INSERT INTO produto_categoria (nome) VALUES ('Beleza e Perfumaria');
-INSERT INTO produto_categoria (nome) VALUES ('Livros');
-INSERT INTO produto_categoria (nome) VALUES ('Eletrônicos');
-INSERT INTO produto_categoria (nome) VALUES ('Esportes');
-INSERT INTO produto_categoria (nome) VALUES ('Viagens');
+CREATE OR REPLACE VIEW vw_pedido_status AS (
+    SELECT  pedido_id,
+            status_id,
+            data_criacao
+    FROM pedido_status
+);
 
-INSERT INTO status (nome) VALUES ('Inicial');
-INSERT INTO status (nome) VALUES ('Em análise');
-INSERT INTO status (nome) VALUES ('Em aprovação');
-INSERT INTO status (nome) VALUES ('Aprovado');
-INSERT INTO status (nome) VALUES ('Em produção');
-INSERT INTO status (nome) VALUES ('Pronto para entrega');
-INSERT INTO status (nome) VALUES ('Em rota');
-INSERT INTO status (nome) VALUES ('Em devolução');
-INSERT INTO status (nome) VALUES ('Entregue');
-INSERT INTO status (nome) VALUES ('Cancelado');
+INSERT INTO vw_produto_categoria (nome) VALUES ('Eletrodomésticos') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_produto_categoria (nome) VALUES ('Celulares') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_produto_categoria (nome) VALUES ('Informática') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_produto_categoria (nome) VALUES ('Cama, Mesa e Banho') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_produto_categoria (nome) VALUES ('Música') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_produto_categoria (nome) VALUES ('Beleza e Perfumaria') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_produto_categoria (nome) VALUES ('Livros') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_produto_categoria (nome) VALUES ('Eletrônicos') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_produto_categoria (nome) VALUES ('Esportes') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_produto_categoria (nome) VALUES ('Viagens') ON CONFLICT (nome) DO NOTHING;
 
-/*
-DROP TABLE IF EXISTS produto_categoria CASCADE;
-DROP TABLE IF EXISTS produto CASCADE;
-DROP TABLE IF EXISTS cliente CASCADE;
-DROP TABLE IF EXISTS cliente_endereco CASCADE;
-DROP TABLE IF EXISTS cliente_telefone CASCADE;
-DROP TABLE IF EXISTS status CASCADE;
-DROP TABLE IF EXISTS pedido CASCADE;
-DROP TABLE IF EXISTS pedido_produto CASCADE;
-DROP TABLE IF EXISTS pedido_status CASCADE;
-*/
+INSERT INTO vw_status (nome) VALUES ('Inicial') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_status (nome) VALUES ('Em análise') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_status (nome) VALUES ('Em aprovação') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_status (nome) VALUES ('Aprovado') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_status (nome) VALUES ('Em produção') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_status (nome) VALUES ('Pronto para entrega') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_status (nome) VALUES ('Em rota') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_status (nome) VALUES ('Em devolução') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_status (nome) VALUES ('Entregue') ON CONFLICT (nome) DO NOTHING;
+INSERT INTO vw_status (nome) VALUES ('Cancelado') ON CONFLICT (nome) DO NOTHING;
